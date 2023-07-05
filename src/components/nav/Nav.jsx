f1362d42c6643f43bd20aa2191fe385a2f7284ae
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-07-05 18:56:01 +0200

    Floating Navbar CSS and Nav States using UseState added

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,14 +5,17 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import { RiFolderUserLine  } from 'react-icons/ri';
 import { MdMailOutline } from 'react-icons/md';
+import {useState} from 'react'
+
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><BiBook/></a>
-      <a href="#portfolio"><RiFolderUserLine /></a> {/* Replaced with MdFolder */}
-      <a href="#contact"><MdMailOutline /></a> {/* Replaced with MdMail */}
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === "#" ? 'active' : ""} ><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === "#about" ? 'active' : ""} ><AiOutlineUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === "#experience" ? 'active' : ""} ><BiBook/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === "#portfolio" ? 'active' : ""}><RiFolderUserLine /></a> {/* Replaced with MdFolder */}
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? 'active' : ""}><MdMailOutline /></a> {/* Replaced with MdMail */}
     </nav>
   )
 }
