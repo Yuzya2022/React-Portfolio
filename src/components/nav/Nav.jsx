e962438fdce9640eb4190033f69d622c80ec8bfb
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-07-05 15:32:26 +0200

    Nav component JSX added

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './nav.css'
-
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import { RiFolderUserLine  } from 'react-icons/ri';
+import { MdMailOutline } from 'react-icons/md';
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#portfolio"><RiFolderUserLine /></a> {/* Replaced with MdFolder */}
+      <a href="#contact"><MdMailOutline /></a> {/* Replaced with MdMail */}
+    </nav>
   )
 }
 
