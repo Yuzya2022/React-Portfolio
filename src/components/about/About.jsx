520cf1e2663414421d1e10adb4be889b6633dabb
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-07-06 18:01:29 +0200

    Carousel/Slides created using SwiperJS

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
             <article className="about__card">
               <FaAward className='about__icon' />
               <h5>Experience</h5>
-              <small>1+ Year Working</small>
+              <small>10+ Year Working</small>
             </article>
             <article className="about__card">
               <FiUsers className='about__icon' />
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -8,6 +8,15 @@ import AVTR5 from '../../assets/test6.jpg'
 import AVTR6 from '../../assets/test7.jpg'
 import AVTR7 from '../../assets/test8.jpg'
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const data = [
   {
     avatar: AVTR1,
@@ -17,12 +26,12 @@ const data = [
   {
     avatar: AVTR2,
     name: 'Galina Boyko',
-    review: "We’ve been working with Olga in Loyalty Program “Vlasnyj Rakhunok” in Fozzy Group as CRM managers. It was really pleasant to work with Olga as with a professional so as with a person. She was always structured, attentive to details and with high ability to optimize the processes to make the efficient. In addition I would say that Olga is perfect as a self-starter, but also a good team player. I’m highly recommend Olga as a very valuable and responsible employee.",
+    review: "We’ve been working with Olga in Loyalty Program “Vlasnyj Rakhunok” in Fozzy Group as CRM managers. It was really pleasant to work with Olga as with a professional so as with a person. She was always structured, attentive to details and with high ability to optimize the processes to make the efficient. ",
   },
   {
     avatar: AVTR3,
     name: 'Trinidad Cerri',
-    review: "During our time together, we collaborated on two full stack projects, the Rent-a-Kitchen-app (Airbnb clone) and the DreamJob project, a job search app with 2 user types. Olga's contributions were invaluable, as she consistently delivered high-quality code and displayed a keen eye for detail. Her commitment to delivering excellence was evident in every line of code she wrote.",
+    review: "During our time together, we collaborated on two full stack projects, the Rent-a-Kitchen-app (Airbnb clone) and the DreamJob project, a job search app with 2 user types. Olga's contributions were invaluable, as she consistently delivered high-quality code and displayed a keen eye for detail. ",
   },
   {
     avatar: AVTR4,
@@ -37,12 +46,12 @@ const data = [
   {
     avatar: AVTR6,
     name: 'Emre Ebeturk',
-    review: "Olga is a talented and dedicated individual with a broad skillset. I am confident that she will bring the same diligence, determination, and team spirit to her future roles. Given her technical proficiency, quick learning capabilities, team-oriented mindset, and social skills, I can wholeheartedly recommend her as a valuable addition to any professional environment.",
+    review: "Olga is a talented and dedicated individual with a broad skillset. I am confident that she will bring the same diligence, determination, and team spirit to her future roles. Given her technical proficiency, quick learning capabilities, team-oriented mindset, and social skills.",
   },
   {
     avatar: AVTR7,
     name: 'Rodrigo Hoffmann',
-    review: "I am thrilled to recommend Olga for any web development role! As a classmate in our Web Development Bootcamp, Olga truly stood out from the crowd. Her dedication, seriousness, and unwavering commitment to her work make her a valuable asset to any team. She never gave up until the task was done, going above and beyond to ensure perfection. Olga's exceptional work ethic and ability to quickly grasp complex concepts make her a standout developer.",
+    review: "I am thrilled to recommend Olga for any web development role! As a classmate in our Web Development Bootcamp, Olga truly stood out from the crowd. Her dedication, seriousness, and unwavering commitment to her work make her a valuable asset to any team. She never gave up until the task was done, going above and beyond to ensure perfection.",
   },
 
 ];
@@ -53,21 +62,27 @@ const Testimonials = () => {
       <h5>Recommendations</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
+      <Swiper className="container testimonials__container"
+            // install Swiper modules
+            modules={[Pagination]}
+            spaceBetween={40}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+      >
         {
           data.map(({avatar,name, review, index}) => {
             return (
-              <article key={index} className='testimonial'>
+              <SwiperSlide key={index} className='testimonial'>
                 <div className="client__avatar">
                   <img src={avatar} alt="Avatar" />
                 </div>
                   <h5 className='client__name'>{name}</h5>
                   <small className='client__review'>{review}</small>
-                </article>
+                </SwiperSlide>
             )
           })
         }
-        </div>
+        </Swiper>
     </section>
   )
 }
